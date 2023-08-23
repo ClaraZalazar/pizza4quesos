fd26cff3c635fc7fa1e3c0a7ef0b40f419984287
ClaraZalazar <zalazarmariaclara@gmail.com>
2023-08-23 14:45:14 -0300

    modifique y se ve bien home, tenia doble un partial de slider, todavia se me rompe al entrar al formEdit y ver detalle, que antes me andaba bien, estoy buscando qué es

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,16 +1,3 @@
-// module.exports = {
-//   productDetail: (req, res) => {
-//     return res.render('productDetail');
-//   },
-//   productAdd: (req, res) => {
-//     return res.render('ProductAdd');
-//   },
-//   productEdit: (req, res) => {
-//     return res.render('productEdit');
-//   }
-// };
-
-
 module.exports = {
   detail: require('./products/detail'),
   add: require('./products/add'),
